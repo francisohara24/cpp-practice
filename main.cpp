c489c1e4c44383ff56f68bc7bb6c675f3722f892
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-08-04 16:45:13 +0000

    Initial commit --> added c-style-concatenation-cpp-program

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+//CONCATENATING C-STYLE STRINGS - 8/4/2022
+//
+#include <iostream>
+
+int main() {
+    char stringOfCharacters[] = "I " "will " "be " "concatenated!";
+    printf("%s", stringOfCharacters);
+    return 0;
+}
