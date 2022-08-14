426e08d7012d68ca53bfc20ee3770fdcacdf1062
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-08-14 20:53:15 +0000

    add string-streams.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 //
-//CONCATENATING C-STYLE STRINGS - 8/4/2022
+// STRING STREAMS 8/12/2022.
 //
+
+#include <sstream>
 #include <iostream>
 
-int main() {
-    char stringOfCharacters[] = "I " "will " "be " "concatenated!";
-    printf("%s", stringOfCharacters);
+int main(){
+    //Parsing a string of comma-separated integers using a string stream object.
+    std::stringstream ss("1, 2, 3, 4, 5");
+    int a, b, c, d, e;
+    char ch;
+    ss>>a>>ch>>b>>ch>>c>>ch>>d>>ch>>e;
+    std::cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<std::endl;
+    std::cout<<ss.str();
     return 0;
 }
