220cb5a24ce86206a30285be7513514921d05922
francisohara24 <franciskohara@gmail.com>
2023-06-25 06:23:04 +0000

    add 1-cpp-hello-world.cpp

---
+++ hackerrank/1-cpp-hello-world.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Francis O'Hara Aidoo on 6/25/2023.
+// Link: https://www.hackerrank.com/challenges/cpp-hello-world/problem
+#include <iostream>
+
+int main() {
+    std::cout<<"Hello, World!"<<std::endl;
+    return 0;
+}
