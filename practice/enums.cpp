51c7974293e48e33d159b8d6578d951417299a5b
francisohara24 <franciskohara@gmail.com>
2023-07-15 05:37:38 +0000

    Feature: add enum.cpp
    source file I used to learn about enums in C++.

---
+++ practice/enums.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Francis O'Hara Aidoo on 7/15/2023.
+//
+#include <iostream>
+
+int main() {
+    enum Color {
+        red, orange, yellow, green, blue, indigo, violet
+    };
+
+    Color color1 = orange;
+
+    std::cout << yellow;
+}
