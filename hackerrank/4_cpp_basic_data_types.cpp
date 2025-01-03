3178eebd44fdd26b9b1535d02301280fe9713279
francisohara24 <franciskohara@gmail.com>
2025-01-03 01:08:43 -0500

    feat: add solution for hackerrank/c++ basic datatypes.

--- hackerrank/4_cpp_basic_data_types.cpp
+++ hackerrank/4_cpp_basic_data_types.cpp
@@ -3,6 +3,17 @@
 // Link: https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem?isFullScreen=true
 //
 
-int main() {
+# include <iostream>
 
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    printf("Enter input values separated by spaces:\t");
+    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
+    printf("%d\n%ld\n%c\n%f\n%lf", a, b, c, d, e);
+    return 0;
 }
