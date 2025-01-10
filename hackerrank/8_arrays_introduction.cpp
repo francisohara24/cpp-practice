1199dfd05aeee14036bd6e45b986190bf4f61861
francisohara24 <franciskohara@gmail.com>
2025-01-10 16:37:34 -0500

    feat: add c++ solution for hackerrank/arrays-introduction

--- hackerrank/8_arrays_introduction.cpp
+++ hackerrank/8_arrays_introduction.cpp
@@ -1,3 +1,23 @@
 //
 // Created by franc on 1/10/2025.
+// Link: https://www.hackerrank.com/challenges/arrays-introduction/problem?isFullScreen=true
 //
+
+#include <iostream>
+
+int main() {
+    int size;
+    scanf("%d", &size);
+
+    int arr[size];
+
+    for (int i = 0; i < size; i ++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < size; i ++) {
+        const char * format_string = (i == size - 1)?"%d": "%d ";
+        printf(format_string, arr[size - i - 1]);
+    }
+    return 0;
+}
