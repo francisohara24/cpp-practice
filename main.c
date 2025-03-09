433241c6c6199236fbaf34ac4ac8d894056c1a51
francisohara24 <franciskohara@gmail.com>
2025-03-09 08:38:18 -0400

    feat: add  file for quickly testing out C code.

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * File Name:   main.c
+ * Description: File for quickly testing out C code.
+ * Author:      Francis O'Hara
+ * Date:        3/8/25
+ */
+
+#include <stdio.h>
+
+int main() {
+    return 0;
+}
